8b379e65f239a1c4cf36ca94eaa936bfb9f01753
1amgriff <maxblogger2001@gmail.com>
2016-12-04 01:05:42 +0700

    Add files via upload

---
+++ thumbnailquality.js
@@ -0,0 +1,6 @@
+﻿function replaceText(){if(!document.getElementById){return;}
+bodyText = document.getElementById("Blog1");
+theText = bodyText.innerHTML;
+theText = theText.replace(/s72-c\//gi, "");
+bodyText.innerHTML = theText;
+}replaceText();
